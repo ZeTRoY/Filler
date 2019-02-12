d9e03eadb20aab7eea802bb4513a3031060135b6
Anton ROI <aroi@e1r6p15.unit.ua>
2019-02-12 19:26:39 +0200

    [Upd] - added piece confirming. Now it can take and write inside a structure pieces

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aroi <aroi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 13:37:58 by aroi              #+#    #+#             */
-/*   Updated: 2019/02/04 14:12:23 by aroi             ###   ########.fr       */
+/*   Updated: 2019/02/12 19:19:36 by aroi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,57 @@ static void	exit_func(char *str)
 {
 	ft_putendl(str);
 	exit (1);
+}
+
+static void get_piece(char *line, t_filler *filler)
+{
+	int i;
+	int j;
+
+	get_next_line(1, &line);
+	if (ft_strnequ(line, "Piece", 5) == 0)
+		return ;
+	if (*(line + 6) >= '0' && *(line + 6) <= '9')
+		if ((filler->piece->y = ft_atoi(line + 6)) <= 0)
+			return ;
+	if (*(line + 7 + ft_count_digits_base(filler->piece->y, 10)) >= '0' && 
+		*(line + 7 + ft_count_digits_base(filler->piece->y, 10)) <= '9')
+		if ((filler->piece->x = ft_atoi(line + 7 +
+			ft_count_digits_base(filler->piece->y, 10))) <= 0)
+		{
+			filler->piece->y = 0;
+			return ;
+		}
+	filler->piece->piece = (char **)malloc(sizeof(char *) * filler->piece->y);
+	i = 0;
+	ft_strdel(&line);
+	while (i < filler->piece->y)
+	{
+		get_next_line(1, &line); //error handle
+		filler->piece->piece[i++] = ft_strdup(line);
+		ft_strdel(&line);
+	}
+}
+
+static void	get_map(char *line, t_filler *filler)
+{
+	int i;
+	int j;
+	int k;
+
+	j = 0;
+	filler->map = (char **)malloc(sizeof(char *) * filler->m);
+	while (j < filler->m)
+	{
+		i = 0;
+		k = 0;
+		get_next_line(1, &line); //error handle
+		while(*(line + i) >= '0' && *(line + i) <= '9')
+			i++;
+		i++;
+		filler->map[j++] = ft_strdup(line + i);
+		ft_strdel(&line);
+	}
 }
 
 static int	get_plateau(char *line, t_filler *filler)
@@ -34,7 +85,7 @@ static int	get_plateau(char *line, t_filler *filler)
 		ft_strdel(&line);
 		return (0);
 	}
-	filler->x = ft_atoi(line + 8);
+	filler->m = ft_atoi(line + 8);
 	while (*(line + 8 + i) >= '0' && *(line + 8 + i) <= '9')
 		i++;
 	if (++i && (*(line + 8 + i) < '0' || *(line + 8 + i) > '9'))
@@ -42,38 +93,9 @@ static int	get_plateau(char *line, t_filler *filler)
 		ft_strdel(&line);
 		return (0);
 	}
-	filler->y = ft_atoi(line + 8 + i);
+	filler->n = ft_atoi(line + 8 + i);
 	ft_strdel(&line);
 	return (1);
-}
-
-static void	get_map(char *line, t_filler *filler)
-{
-	int i;
-	int j;
-	int k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	// filler->map = (int **)malloc(sizeof(int *) * filler->x);
-	while (j < filler->x - 1)
-	{
-		get_next_line(1, &line); //error handle
-		// while(*(line + i) >= '0' && *(line + i) <= '9')
-		// 	i++;
-		// i++;
-		// filler->map[j] = (int *)malloc(sizeof(int) * filler->y);
-		// while (*(line + i))
-		// {
-		// 	if (*(line + i++) != filler->playermark)
-		// 		filler->map[j][k++] = 0;
-		// 	else
-		// 		filler->map[j][k++] = 1;
-		// }
-		ft_strdel(&line);
-		j++;
-	}
 }
 
 static void	check_name(char *line, t_filler *filler)
@@ -98,7 +120,7 @@ static void	check_name(char *line, t_filler *filler)
 		exit_func("Bad player info");
 	while(*(line + 10 + i) == ' ')
 		i++;
-	if (ft_strnequ(line + 10 + i, "aroi", 4) == 0 || *(line + 14 + i) != '\0')
+	if (ft_strstr(line + 10 + i, "aroi.filler") != NULL)
 		exit_func("Bad player info");
 }
 
@@ -120,8 +142,34 @@ int			main(void)
 		get_next_line(1, &line);		//get the line with 01234... etc
 		ft_strdel(&line);
 		get_map(line, filler);			// malloced map
-		ft_strdel(&line);
+		get_piece(line, filler);
 	}
-	system("leaks aroi.filler");
+	int i = 0;
+	while (i < filler->m)
+	{
+	// int j = 0;
+	// 	while (j < filler->n)
+	// 	{
+	// 		ft_printf("%2d", filler->map[i][j]);
+	// 		j == filler->n - 1 ? write(1, "\n", 1) : 0;
+	// 		j++;
+	// 	}
+		ft_printf("%s\n", filler->map[i]);
+		i++;
+	}
+	i = 0;
+	while (i < filler->piece->y)
+	{
+	// int j = 0;
+	// 	while (j < filler->n)
+	// 	{
+	// 		ft_printf("%2d", filler->map[i][j]);
+	// 		j == filler->n - 1 ? write(1, "\n", 1) : 0;
+	// 		j++;
+	// 	}
+		ft_printf("%s\n", filler->piece->piece[i]);
+		i++;
+	}
+	// system("leaks aroi.filler");
 	return (0);
 }
--- src/t_filler.c
+++ src/t_filler.c
@@ -1,13 +1,20 @@
 #include "filler.h"
 
+// t_piece			
+
 t_filler		*ft_create_filler(void)
 {
 	t_filler	*new;
 
-	new = (t_filler *)malloc(sizeof(t_filler));
+	if ((new = (t_filler *)malloc(sizeof(t_filler))) == NULL)
+		return (NULL);
 	new->playermark = 0;
-	new->x = 0;
-	new->y = 0;
+	new->m = 0;
+	new->n = 0;
 	new->map = NULL;
+	if ((new->piece = (t_piece *)malloc(sizeof(t_piece))) == NULL)
+		return (NULL); //ft_strdel!
+	new->piece->x = 0;
+	new->piece->y = 0;
 	return (new);
 }
